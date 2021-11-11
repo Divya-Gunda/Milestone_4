b733b52f6bc332550a62282e3cb85ca07aa758ac
unknown <minddevops07@iiht.tech>
2021-11-11 14:19:15 +0530

    adding hospital management file

---
+++ src/main/java/com/springexample/springApp_jfrog/SpringAppJfrogApplication.java
@@ -0,0 +1,13 @@
+package com.springexample.springApp_jfrog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppJfrogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppJfrogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springexample/springApp_jfrog/SpringAppJfrogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springexample.springApp_jfrog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppJfrogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
